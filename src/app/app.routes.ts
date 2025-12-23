3a1e011d18ca1fdd47e4cb8475e7113022b7feab
diopmorfall <morfall.papydiop99@gmail.com>
2025-12-23 18:37:35 +0100

    Basic landing page setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './components/landing/landing-page/landing-page';
 
 export const routes: Routes = [
-    
+    { path: '', component: LandingPage }
 ];
---
+++ src/app/components/landing/landing-page/landing-page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-landing-page',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './landing-page.html',
+    styleUrl: './landing-page.css',
+})
+export class LandingPage {
+
+}
