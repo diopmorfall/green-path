377f6fe8ac38beba297c29e57d8b8bbe6c481c56
diopmorfall <morfall.papydiop99@gmail.com>
2026-01-21 15:46:21 +0100

    Selects components for business model and company size added

--- src/app/features/auth/sign-up/components/company-info-form/company-info-form.ts
+++ src/app/features/auth/sign-up/components/company-info-form/company-info-form.ts
@@ -3,11 +3,12 @@ import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, AbstractContro
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
     selector: 'app-company-info-form',
     standalone: true,
-    imports: [ReactiveFormsModule, CommonModule, MatFormFieldModule, MatInputModule],
+    imports: [ReactiveFormsModule, CommonModule, MatFormFieldModule, MatInputModule, MatSelectModule],
     templateUrl: './company-info-form.html',
     styleUrl: './company-info-form.css',
 })
@@ -17,12 +18,20 @@ export class CompanyInfoForm {
     companyInfoFormGroup: FormGroup;
     EU_VAT_REGEX = new RegExp('^(ATU[0-9]{8}|BE[01][0-9]{9}|BG[0-9]{9,10}|HR[0-9]{11}|CY[A-Z0-9]{9}|CZ[0-9]{8,10}|DK[0-9]{8}|EE[0-9]{9}|FI[0-9]{8}|FR[0-9A-Z]{2}[0-9]{9}|DE[0-9]{9}|EL[0-9]{9}|HU[0-9]{8}|IE([0-9]{7}[A-Z]{1,2}|[0-9][A-Z][0-9]{5}[A-Z])|IT[0-9]{11}|LV[0-9]{11}|LT([0-9]{9}|[0-9]{12})|LU[0-9]{8}|MT[0-9]{8}|NL[0-9]{9}B[0-9]{2}|PL[0-9]{10}|PT[0-9]{9}|RO[0-9]{2,10}|SK[0-9]{10}|SI[0-9]{8}|ES[A-Z]([0-9]{8}|[0-9]{7}[A-Z])|SE[0-9]{12}|GB([0-9]{9}|[0-9]{12}|GD[0-4][0-9]{2}|HA[5-9][0-9]{2}))$')
     //shape-only VAT validator, proper VIES validation is needed
+    companyBusinessModels: string[] = [
+        "SaaS", "Software House", "Startup", "Digital marketing/creative agency"
+    ]
+    companySizes: string[] = [
+        "Micro (1-9 employees)",
+        "Small (10-49 employees)",
+        "Medium (50-249 employees)"
+    ]
 
     constructor(private formBuilder: FormBuilder) {
         this.companyInfoFormGroup = this.formBuilder.group({
             companyName: ['', [Validators.required]],
             vatNumber: ['', [Validators.required, this.euVatValidator()]],
-            industry: ['', [Validators.required]],
+            companyBusinessModel: ['', [Validators.required]],
             companySize: ['', [Validators.required]]
         });
     }
